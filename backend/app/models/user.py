3702116b54b73f3d9b4e8025632be5de3e049f31
Adeyi Esther <adeyiesther01@gmail.com>
2026-04-15 16:33:26 +0100

    set up user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Enum
+from app.database import Base
+import enum
+
+
+class UserRole(enum.Enum):
+    user = "user"
+    admin = "admin"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(UserRole), default=UserRole.user, nullable=False)
