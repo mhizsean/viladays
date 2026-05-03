9fefa6c6518d5a3f2c3c12fcc4e0aacbf50b3d20
Adeyi Esther <adeyiesther01@gmail.com>
2026-05-03 23:25:18 +0100

    itinerary page created

---
+++ frontend/src/pages/ItineraryPage.tsx
@@ -0,0 +1,10 @@
+const ItineraryPage = () => {
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-8">
+      <h1>My Plans</h1>
+      <p>Your personalized trip plans</p>
+    </div>
+  );
+};
+
+export default ItineraryPage;
